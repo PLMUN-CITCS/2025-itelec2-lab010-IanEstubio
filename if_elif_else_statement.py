c3ac6b4ce7fd011112cff50b9d91a12ddbcec700
IanEstubio <kazuhajean27@gmail.com>
2025-02-14 17:03:57 +0800

    Submitting Python Week 04 - Laboratory # 10

---
+++ if_elif_else_statement.py
@@ -0,0 +1,31 @@
+# IAN JAY P. ESTUBIO 
+# Week 04 - Conditional Statements
+# Laboratory # 10 - Guided Coding Exercise: if...elif...else Statement in Python
+
+def main():
+    """Main function to check if the number is even or odd."""
+    
+    try:
+        user_input = input("Enter your numeric grade: ")
+        grade = int(user_input)
+
+        # Code to check even or odd
+        if grade >= 90:
+           letter_grade = "A"
+        elif grade >= 80:
+           letter_grade = "B"
+        elif grade >= 70:
+           letter_grade = "C"
+        elif grade >= 60:
+           letter_grade = "D"
+        else:
+           letter_grade = "F"
+    
+        print("Your grade is:", letter_grade)
+    
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
+
+if __name__ == "__main__":
+    main()
+
